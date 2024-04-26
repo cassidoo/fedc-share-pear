d905c07cd681d0c8183abee958f96c996c85fc69
Cassidy Williams <1454517+cassidoo@users.noreply.github.com>
2024-04-25 23:24:53 -0400

    Just accept everything

--- netlify/edge-functions/headers.js
+++ netlify/edge-functions/headers.js
@@ -1,25 +1,17 @@
 export default async (request, context) => {
 	const response = await context.next();
 
-	// Only allow requests from our own domains
-	let url = request.headers.get("origin");
-
-	let allowedUrlsRegex =
-		/^(https?:\/\/)?(localhost(:\d+)?|https?:\/\/(?:www\.)?codepen\.io\/\w+\/(?:pen|live)\/\w+\S*|https?:\/\/(?:www\.)?cdpn\.io\/\w+\/fullpage\/\w+\S*)$/;
-
 	// Handle preflight requests
-	if (request.method === "OPTIONS" && allowedUrlsRegex.test(url)) {
+	if (request.method === "OPTIONS") {
 		return new Response("ok", {
 			headers: {
-				"Access-Control-Allow-Origin": url,
+				"Access-Control-Allow-Origin": "*",
 				"Access-Control-Allow-Headers": "Content-Type, markdown"
 			}
 		});
 	}
 
-	if (url && allowedUrlsRegex.test(url)) {
-		response.headers.set("Access-Control-Allow-Origin", url);
-	}
+	response.headers.set("Access-Control-Allow-Origin", "*");
 
 	// Accept markdown header
 	response.headers.set(
